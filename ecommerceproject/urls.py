de747ee037190c4544f892d0761689931f328514
Akhil-pm <akhil9997@gmail.com>
2021-09-28 13:40:44 +0530

    added feature to search a product

--- ecommerceproject/urls.py
+++ ecommerceproject/urls.py
@@ -22,6 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/', include('shop.urls')),
+    path('search/', include('search_app.urls')),
 ]
 
 
