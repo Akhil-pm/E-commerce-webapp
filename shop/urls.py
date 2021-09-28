ff26ecd97799f813ecc1f336b4ddafb48a1685e9
Akhil-pm <akhil9997@gmail.com>
2021-09-28 12:32:20 +0530

    added individual product page and done major styling

--- shop/urls.py
+++ shop/urls.py
@@ -9,4 +9,5 @@ app_name='shop'
 urlpatterns = [
       path('',views.allProdcat,name='allProdCat'),
       path('<slug:c_slug>/',views.allProdcat,name='products_by_category'),
+      path('<slug:c_slug>/<slug:product_slug>/',views.proDetail,name='prod_cat_detail'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -20,3 +20,11 @@ def allProdcat(request,c_slug=None):
     except(EmptyPage, InvalidPage):
         products = paginator.page(paginator.num_pages)
     return render(request, "category.html", {'category': c_page, 'products': products})
+
+
+def proDetail(request,c_slug,product_slug):
+    try:
+        product=Product.objects.get(category__slug=c_slug,slug=product_slug)
+    except Exception as e:
+        raise e
+    return render(request,'product.html',{'product':product})
