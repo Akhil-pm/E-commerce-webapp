abbc4e12d35b191916d3f1e97fee621f0843bc97
Akhil-pm <akhil9997@gmail.com>
2021-09-28 18:04:30 +0530

    functionality to add products into cart and view cart details

--- ecommerceproject/urls.py
+++ ecommerceproject/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/', include('shop.urls')),
     path('search/', include('search_app.urls')),
+    path('cart/', include('cart.urls')),
 ]
 
 
